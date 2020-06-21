5c5d333b64eabfbd7ae85c4bac053d16695bb7f4
harsonlau <harsonlau@gmail.com>
2020-06-21 11:19:09 +0800

    remove build
    Because:
    - Explain the reasons you made this change
    - Make a new bullet for each reason
    - Each line should be under 72 characters
    
    Explain exactly what was done in this commit with more depth than the
    50 character subject line. Remember to wrap at 72 characters!
    
    Include any additional notes, relevant links, or co-authors.

--- build/jsoncpp-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/harson/p2/3rdparty/jsoncpp
-# Build directory: /home/harson/p2/build/jsoncpp-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("include")
--- build/jsoncpp-build/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/harson/p2/3rdparty/jsoncpp/include
-# Build directory: /home/harson/p2/build/jsoncpp-build/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
